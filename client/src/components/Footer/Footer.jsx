0126a1c508ceebb4aecdcdb6d1e8ccc12fe335e8
artfront5 <art.front@list.ru>
2022-06-10 13:35:57 +0300

    Footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>
+        <span class="styling">Команда разработчиков: </span>Антон, Сергей, Алекс, Артем (ASAA)
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
