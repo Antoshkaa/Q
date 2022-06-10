112c0b63e01dac02dc6bdfa6007ba305adf2b6b3
MrST-7 <strukhachev7@gmail.com>
2022-06-10 10:50:18 +0300

    sliderIsReady

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import HomePage from "../HomePage/HomePage";
 import Login from "../Login/Login";
 import Navb from "../Navb/Navb";
 import Registration from "../Registration/Registration";
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navb />
       <Routes>
+        <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
       </Routes>
