c0ba3db651ca414f39a31d5d60b226feef419f62
Anton <anton.yasinyuk@mail.ru>
2022-06-10 13:40:58 +0300

    csrd

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,6 +3,7 @@ import HomePage from "../HomePage/HomePage";
 import Login from "../Login/Login";
 import Navb from "../Navb/Navb";
 import Registration from "../Registration/Registration";
+// import GamePole from "../GamePole/GamePole.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
+        {/* <Route path="/game" element={<GamePole />} /> */}
       </Routes>
     </div>
   );
---
+++ client/src/components/GamePole/GamePole.jsx.jsx
@@ -0,0 +1,14 @@
+import {useDispatch} from 'react-redux';
+import React from 'react';
+import OneCard from '../OneCard/OneCard';
+
+const GamePoleJsx = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default GamePoleJsx;
